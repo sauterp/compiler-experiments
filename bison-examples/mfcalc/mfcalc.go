af65517fe1272f1e5cdbdc77839d377d7b173982
Philipp Sauter <sauterp@protonmail.com>
2020-12-25 11:18:07 +0100

    wip multi-function calculator

--- bison-examples/mfcalc/mfcalc.go
+++ bison-examples/mfcalc/mfcalc.go
@@ -1,14 +1,13 @@
 package main
 
 // SymRec is a data type for links in the chain of symbols.
-type SymRec struct
-{
-  Name *byte  /* name of symbol                     */
-  Type int    /* type of symbol: either VAR or FNCT */
-  Value struct {
-	Var float64
-	FnctPtr *func() float64
-  }
+type SymRec struct {
+	Name  *byte /* name of symbol                     */
+	Type  int   /* type of symbol: either VAR or FNCT */
+	Value struct {
+		Var     float64
+		FnctPtr *func() float64
+	}
 	Next *SymRec
 }
 
@@ -18,55 +17,55 @@ func main() {
 }
 
 /* Called by yyparse on error */
-func yyerror(s string){
+func yyerror(s string) {
 	fmt.Printf("%s\n", s)
 }
 
 type Init struct {
 	FName string
-	Fnct *func() float64
+	Fnct  *func() float64
 }
 
-var ArithFncts = []Init {
-      {"sin", sin},
-      {"cos", cos},
-      {"atan", atan},
-      {"ln", log},
-      {"exp", exp},
-      {"sqrt", sqrt},
-      {0, 0},
-    }
+var ArithFncts = []Init{
+	{"sin", sin},
+	{"cos", cos},
+	{"atan", atan},
+	{"ln", log},
+	{"exp", exp},
+	{"sqrt", sqrt},
+	{0, 0},
+}
 
 /* The symbol table: a chain of `struct SymRec'.  */
 var SymRec *SymTable
 
 //InitTable puts arithmetic functions in table.
 func InitTable() {
-  var i int
-  var ptr *SymRec
+	var i int
+	var ptr *SymRec
 	for i := 0; ArithFncts[i].FName != 0; i++ {
 		ptr = PutSym(ArithFncts[i].FName, FNCT)
 		ptr.value.fnctptr = ArithFncts[i].fnct
 	}
 }
 
-func PutSym(SymName string,SymType int) *SymRec {
-  var ptr *SymRec
+func PutSym(SymName string, SymType int) *SymRec {
+	var ptr *SymRec
 	ptr = new(SymRec)
 	ptr.Name = SymName
-  ptr.Type = SymType
-  ptr.Value.Var = 0; /* set value to 0 even if fctn.  */
+	ptr.Type = SymType
+	ptr.Value.Var = 0 /* set value to 0 even if fctn.  */
 	ptr.Next = symTable
-  symTable = ptr
-  return ptr
+	symTable = ptr
+	return ptr
 }
 
-func GetSym (SymName string) *SymRec {
-  var ptr *SymRec
-  for ptr = symTable; ptr != nil; ptr = ptr.Next {
-    if strings.Compare(ptr.name,SymName) == 0 {
-      return ptr
+func GetSym(SymName string) *SymRec {
+	var ptr *SymRec
+	for ptr = symTable; ptr != nil; ptr = ptr.Next {
+		if strings.Compare(ptr.name, SymName) == 0 {
+			return ptr
+		}
 	}
-}
-  return nil
+	return nil
 }
